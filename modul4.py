5c14942ad4577f99fdcd70bd0905df4cb72aa7f8
Paul02111995 <romanyk02111995@gmail.com>
2024-04-06 22:47:01 +0300

    Task4

--- modul4.py
+++ modul4.py
@@ -36,4 +36,71 @@ def get_cats_info(path):
 cats_info = get_cats_info("/Applications/homework4/cats.txt")
 print(cats_info)
 
+# Task 4 
+def parse_input(user_input):
 
+  cmd, *args = user_input.split()
+  cmd = cmd.strip().lower()
+  return cmd, *args
+
+
+def add_contact(args, contacts):
+  name, phone = args
+  contacts[name] = phone
+  return "Contact added."
+
+
+def change_contact(args, contacts):
+  name, phone = args
+  if name not in contacts:
+    return f"Contact '{name}' not found."
+  contacts[name] = phone
+  return "Contact updated."
+
+
+def show_phone(name, contacts):
+  if name not in contacts:
+    return f"Contact '{name}' not found."
+  return contacts[name]
+
+
+def show_all(contacts):
+  if not contacts:
+    return "No contacts found."
+  return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
+
+
+def main():
+  
+  contacts = {}
+
+  print("Welcome to the assistant bot!")
+  while True:
+    user_input = input("Enter a command: ")
+    command, *args = parse_input(user_input)
+
+    if command in ["close", "exit"]:
+      print("Good bye!")
+      break
+
+    elif command == "hello":
+      print("How can I help you?")
+
+    elif command == "add":
+      print(add_contact(args, contacts))
+
+    elif command == "change":
+      print(change_contact(args, contacts))
+
+    elif command == "phone":
+      print(show_phone(*args, contacts))
+
+    elif command == "all":
+      print(show_all(contacts))
+
+    else:
+      print("Invalid command.")
+
+
+if __name__ == "__main__":
+  main()
